364c8e6beec067f56183ca6e6991151683125016
Debarshi Ray <rishi@gnu.org>
2009-05-20 02:49:32 +0530

    Fixed copyright and license notices.
    It is better to use the program name 'Solang' instead of the names of
    the individual files in the notices because it reduces maintenance when
    file names change. Also, put the name of the author after the copyright
    year and remove trailing whitespaces.

--- src/common/plugin.cpp
+++ src/common/plugin.cpp
@@ -1,18 +1,17 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
- * plugin.cpp
- * Copyright (C) Debarshi Ray 2009 <rishi@gnu.org>
- * 
- * plugin.cpp is free software: you can redistribute it and/or modify it
+ * Copyright (C) 2009 Debarshi Ray <rishi@gnu.org>
+ *
+ * Solang is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * plugin.cpp is distributed in the hope that it will be useful, but
+ *
+ * Solang is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
--- src/common/progress-observer.cpp
+++ src/common/progress-observer.cpp
@@ -1,18 +1,17 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
- * progress-observer.cpp
- * Copyright (C) Santanu Sinha 2009 <santanu.sinha@gmail.com>
- * 
- * progress-observer.cpp is free software: you can redistribute it and/or modify it
+ * Copyright (C) 2009 Santanu Sinha <santanu.sinha@gmail.com>
+ *
+ * Solang is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * progress-observer.cpp is distributed in the hope that it will be useful, but
+ *
+ * Solang is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
